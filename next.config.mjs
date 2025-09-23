69905fc680661b9152410e0a1e368a93688361a2
Kervin Philippe <kervin@blexitek.com>
2025-09-23 19:18:16 -0400

    fixing deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    webpack: (config) => {
-      config.externals.push("pino-pretty", "lokijs", "encoding");
-      return config;
-    },
-  };
+  compress: true,
+  poweredByHeader: false,
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+};
 
 export default nextConfig;
